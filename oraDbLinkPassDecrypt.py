b4b1824e5bc7aef035930395d65bf4f427d2d60f
mustafasherazi <mustafasherazi@gmail.com>
2014-08-19 19:49:03 -0500

    Fixed some bugs

--- oraDbLinkPassDecrypt.py
+++ oraDbLinkPassDecrypt.py
@@ -7,20 +7,22 @@ from binascii import unhexlify
 
 def usage():
     print ("select userid||':'||passwordx from sys.link$;")
-    print ("oraDBLinkPassDecrypter.py <oracle db version 11|12> USERID:PASSWORDX")
+    print ("oraDBLinkPassDecrypter.py 11 USERID:PASSWORDX")
 
-if sys.argv[1] and sys.argv[2]:
+def pDecrypt(userid, hexpass):
+    key = unhexlify(hexpass)[1:9]
+    encryptedpwd = unhexlify(hexpass)[9:]
+    IV='\0\0\0\0\0\0\0\0' #Set IV to 0
+    d=DES.new(key, DES.MODE_CBC, IV)
+    return ("Decrypted password of %s is %s" % (userid, d.decrypt(encryptedpwd)))
+
+if len(sys.argv) == 3 and sys.argv[1] and sys.argv[2]:
     if sys.argv[1] <= '11':
         userid = sys.argv[2].split(':')[0]
         hexpass = sys.argv[2].split(':')[1]
+        print (pDecrypt(userid, hexpass))
     elif sys.argv[1] == '12':
         print ("Missing implementation")
 else:
     #check_tnsnames()
     usage()
-
-key = unhexlify(hexpass)[1:9]
-encryptedpwd = unhexlify(hexpass)[9:]
-IV='\0\0\0\0\0\0\0\0' #Set IV to 0
-d=DES.new(key, DES.MODE_CBC, IV)
-print ("Decrypted password of %s is %s" % (userid, d.decrypt(encryptedpwd)))
